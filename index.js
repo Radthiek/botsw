8a8c0b8cc4a9e5f1a32e4e343858317ed79c667e
Radthiek <44319561+Radthiek@users.noreply.github.com>
2018-11-09 15:23:42 +0100

    Update index.js

--- index.js
+++ index.js
@@ -48,44 +48,7 @@ client.on('message', message => { // RadThiek
 });
 
 
-client.on("ready", () => {
-    var guild;
-    while (!guild)
-        guild = client.guilds.get("510087212726091776")// ايدي السيرفر
-    guild.fetchInvites().then((data) => {
-        data.forEach((Invite, key, map) => {
-            var Inv = Invite.code;
-            dat[Inv] = Invite.uses;
-        })
-    })
-})
 
-
-
-client.on("guildMemberAdd", (member) => {
-    let channel = member.guild.channels.get("510088193584922638");// ايدي الروم
-    if (!channel) {
-        console.log("!channel fails");
-        return;
-    }
-    if (member.id == client.user.id) {
-        return;
-    }
-    console.log('made it till here!');
-    var guild;
-    while (!guild)
-        guild = client.guilds.get("510087212726091776")// ايدي السيرفر
-    guild.fetchInvites().then((data) => {
-        data.forEach((Invite, key, map) => {
-            var Inv = Invite.code;
-            if (dat[Inv])
-                if (dat[Inv] < Invite.uses) {
- channel.send(`**Join By :**  ${Invite.inviter}`)            
- }
-            dat[Inv] = Invite.uses;
-        })
-    })
-})
 
 client.on('message', msg => {
   if (msg.content === '.restart') {
